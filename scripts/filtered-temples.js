039868299798d87e1ca0cdaa37f7d1b67b593319
almabarraza <abarrazaraygoza@byupathway.edu>
2025-04-01 22:07:02 -0700

    week 04 finished

---
+++ scripts/filtered-temples.js
@@ -0,0 +1,196 @@
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    hamButton.classList.toggle('open');
+});
+
+const year = document.querySelector("#currentyear");
+
+const today = new Date();
+
+year.innerHTML = `<span class="highlight">${today.getFullYear()}</span>`;
+
+document.getElementById("lastModified").textContent = document.lastModified;
+
+const temples = [
+    {
+        templeName: "Aba Nigeria",
+        location: "Aba, Nigeria",
+        dedicated: "2005, August, 7",
+        area: 11500,
+        imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/aba-nigeria/400x250/aba-nigeria-temple-lds-273999-wallpaper.jpg"
+    },
+    {
+        templeName: "Manti Utah",
+        location: "Manti, Utah, United States",
+        dedicated: "1888, May, 21",
+        area: 74792,
+        imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/manti-utah/400x250/manti-temple-768192-wallpaper.jpg"
+    },
+    {
+        templeName: "Payson Utah",
+        location: "Payson, Utah, United States",
+        dedicated: "2015, June, 7",
+        area: 96630,
+        imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/payson-utah/400x225/payson-utah-temple-exterior-1416671-wallpaper.jpg"
+    },
+    {
+        templeName: "Yigo Guam",
+        location: "Yigo, Guam",
+        dedicated: "2020, May, 2",
+        area: 6861,
+        imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/yigo-guam/400x250/yigo_guam_temple_2.jpg"
+    },
+    {
+        templeName: "Washington D.C.",
+        location: "Kensington, Maryland, United States",
+        dedicated: "1974, November, 19",
+        area: 156558,
+        imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/washington-dc/400x250/washington_dc_temple-exterior-2.jpeg"
+    },
+    {
+        templeName: "Lima Perú",
+        location: "Lima, Perú",
+        dedicated: "1986, January, 10",
+        area: 9600,
+        imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/lima-peru/400x250/lima-peru-temple-evening-1075606-wallpaper.jpg"
+    },
+    {
+        templeName: "Mexico City Mexico",
+        location: "Mexico City, Mexico",
+        dedicated: "1983, December, 2",
+        area: 116642,
+        imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/mexico-city-mexico/400x250/mexico-city-temple-exterior-1518361-wallpaper.jpg"
+    },
+
+    {
+        templeName: "Buenos Aires Argentina",
+        location: "Evita city, Buenos Aires Argentina",
+        dedicated: "1986, January, 17-19",
+        area: 30659,
+        imageUrl:
+            "https://churchofjesuschristtemples.org/assets/img/temples/buenos-aires-argentina-temple/buenos-aires-argentina-temple-4088.jpg"
+    },
+
+
+    {
+        templeName: "Mesa Arizona",
+        location: "Mesa Arizona, United States",
+        dedicated: "1927, October 23-26",
+        area: 75000,
+        imageUrl:
+            "https://churchofjesuschristtemples.org/assets/img/temples/mesa-arizona-temple/mesa-arizona-temple-186.jpg"
+
+    },
+
+
+
+    {
+        templeName: "Bogotá Colombia",
+        location: "Bogotá, Distrito Capital Colombia",
+        dedicated: "1999, April 24-26",
+        area: 53500,
+        imageUrl:
+            "https://churchofjesuschristtemples.org/assets/img/temples/bogota-colombia-temple/bogota-colombia-temple-7740.jpg"
+
+    },
+
+];
+
+
+createTempleCard(temples);
+
+const oldLink = document.querySelector("#old");
+
+oldLink.addEventListener("click", () => {
+
+    let yearFilter = 1900
+    let templesYear = temples.map(temple => {
+        let year = temple.dedicated.split(",")[0].trim();
+        return {
+            ...temple,
+            yearDedicated: parseInt(year)
+        };
+    });
+
+    createTempleCard(templesYear.filter(temple => temple.yearDedicated <= yearFilter));
+
+}
+)
+
+const newLink = document.querySelector("#new");
+
+newLink.addEventListener("click", () => {
+
+    let yearFilterNew = 2000
+    let templesYearNew = temples.map(temple => {
+        let yearNew = temple.dedicated.split(",")[0].trim();
+        return {
+            ...temple,
+            yearDedicated: parseInt(yearNew)
+        };
+    });
+
+    createTempleCard(templesYearNew.filter(temple => temple.yearDedicated > yearFilterNew));
+
+});
+
+const large = document.querySelector("#large");
+large.addEventListener("click", () => {
+    createTempleCard(temples.filter(temple => temple.area >= 90000));
+})
+
+const small = document.querySelector("#small");
+small.addEventListener("click", () => {
+    createTempleCard(temples.filter(temple => temple.area <= 10000));
+})
+
+const home = document.querySelector("#home");
+home.addEventListener("click", () => {
+    createTempleCard(temples);
+});
+
+
+
+function createTempleCard(filteredTemples) {
+    document.querySelector(".res-grid").innerHTML = "";
+    filteredTemples.forEach(temple => {
+
+        let card = document.createElement("section");
+        let name = document.createElement("h3");
+        let location = document.createElement("p");
+        let dedication = document.createElement("p");
+        let area = document.createElement("p");
+        let img = document.createElement("img");
+
+        name.textContent = temple.templeName;
+        location.innerHTML = `<span class="label">Location:</span> ${temple.location}`;
+        dedication.innerHTML = `<span class="label">Dedicated:</span>${temple.dedicated}`;
+        area.innerHTML = `<span class="label">Size:</span>${temple.area} sq ft`;
+        img.setAttribute("src", temple.imageUrl);
+        img.setAttribute("alt", `${temple.templeName} Temple`);
+        img.setAttribute("loading", "lazy");
+
+
+
+        card.appendChild(name);
+        card.appendChild(location);
+        card.appendChild(dedication);
+        card.appendChild(area);
+        card.appendChild(img);
+
+        document.querySelector(".res-grid").appendChild(card);
+
+    })
+
+
+}
